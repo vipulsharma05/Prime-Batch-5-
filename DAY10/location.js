55b8a04f599d88f794000b3c2dd2384f66fd19f6
VIPULSHARMA <vipulsharma5002@gmail.com>
2025-02-27 23:55:35 +0530

    day10 finish

--- DAY10/location.js
+++ DAY10/location.js
@@ -15,7 +15,7 @@ const getData = (text) => {
     })
 };
 getData()
-const root = document.getElementById('location')
+const root = document.getElementById('locations')
 const showLocations = (data)=>{
     root.innerHTML = ''
     const {predictions} = data
@@ -26,7 +26,42 @@ const showLocations = (data)=>{
     });
     console.log(data)
 }
+let id = null;
 
-const handleSearch=(e)=>{
-    getData(e.target.value)
-}
+const handleSearch = (e) => {
+    if (id) clearTimeout(id);
+
+    id = setTimeout(() => {
+        getData(e.target.value);
+    }, 800);
+};
+
+const getSpecificLocationInfo = (placeId) => {
+    const pr = fetch(
+        `https://google-map-places.p.rapidapi.com/maps/api/place/details/json?place_id=${placeId}&region=en&fields=all&language=en&reviews_no_translations=true`,
+        {
+            method: "GET",
+            headers: {
+                "x-rapidapi-host": "google-map-places.p.rapidapi.com",
+                "x-rapidapi-key": "4334cf89c2msh8bb2842ca1bc925p14d540jsn5476cbded4a8",
+            },
+        }
+    );
+    pr.then((res) => {
+        const pr2 = res.json();
+        pr2.then((data) => {
+            console.log(data);
+            const { result } = data;
+            const rootNew = document.getElementById("location-info");
+            rootNew.innerHTML = `
+                <h4>${result.name}</h4>
+                <p>${result.formatted_address}</p>
+                <img src='${result.icon}'>
+                <a href='${result.url}' target="_blank">Open</a>
+                ${result.photos.map((photoObj) => {
+                    return photoObj.html_attributions[0];
+                })}
+            `;
+        });
+    });
+};
